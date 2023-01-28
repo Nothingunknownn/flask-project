d3dfb11da959067ffb77c7c730759d100d2a8cf9
Kirill Marchenko <kirillvks24@gmail.com>
2023-01-28 18:59:18 +0500

    reform disign, add bootstrap and jquery script, add post page

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
+import sqlite3
+
 from flask import Flask, render_template, flash, redirect
 from flask_wtf.csrf import CSRFProtect
 from forms import LoginForm
+from werkzeug.exceptions import abort
+
 csrf = CSRFProtect()
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Nothing'}
-    posts = [
-        {
-            'author': {'nickname': 'Alex'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+
     return render_template(
         "index.html",
         title="Main page",
-        user=user,
-        posts=posts,
+        posts=posts
+    )
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template(
+        'post.html',
+        post=post
     )
 
 
